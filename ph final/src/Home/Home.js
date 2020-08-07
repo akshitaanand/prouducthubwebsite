1c7b559f0898b428d9233b6d8eb48030e022f1b6
akshitaanand <63819293+akshitaanand@users.noreply.github.com>
2020-08-07 18:28:27 -0500

    Add files via upload

--- ph final/src/Home/Home.js
+++ ph final/src/Home/Home.js
@@ -59,15 +59,15 @@ class Home extends Component {
           <div class="container">
             <ReactPlayer
               className="space2"
-              width="500px"
-              height="320px"
+              width="600px"
+              height="400px"
               url="https://www.youtube.com/watch?v=aS-LUW5Jim0"
             />
             <p className="space3">(talk about producthub here)</p>
           </div>
         </div>
         <div className="first">
-          <h3>Sliding Images</h3>
+          <h3 className="title">Sliding Images</h3>
           <div>
             <CarouselSlider
               slideItems={images}
@@ -80,15 +80,41 @@ class Home extends Component {
         </div>
         <div className="second">
           <h3>Featured Products</h3>
+          <div>
+            <CarouselSlider
+              slideItems={images}
+              manner={manner}
+              buttonSetting={buttonSetting}
+              sliderBoxStyle={sliderBoxStyle}
+              itemsStyle={itemsStyle}
+            />
+          </div>
         </div>
         <div className="third">
           <h3>What makes us different?</h3>
         </div>
         <div className="fourth">
           <h3>Featured Article Cards</h3>
+          <div>
+            <CarouselSlider
+              slideItems={images}
+              manner={manner}
+              buttonSetting={buttonSetting}
+              sliderBoxStyle={sliderBoxStyle}
+              itemsStyle={itemsStyle}
+            />
+          </div>
         </div>
         <div className="fifth">
           <h3>Video about our process</h3>
+          <div>
+          <ReactPlayer
+              className="video"
+              width="2000px"
+              height="400px"
+              url="https://www.youtube.com/watch?v=aS-LUW5Jim0"
+            />
+          </div>
         </div>
         <div className="sixth">
           <h3>Featured Projects</h3>
